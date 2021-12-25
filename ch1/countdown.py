7f6c39eefa8774810e9ad20052cbdb38587d9948
hiro881213 <nikep.godbard@gmail.com>
2021-12-25 21:36:50 +0900

    辞書

--- ch1/countdown.py
+++ ch1/countdown.py
@@ -1,2 +1,23 @@
 for countdown in 5, 4, 3, 2, 1, "hey" :
     print(countdown)
+
+print("-----スペル-----")
+
+spells = [
+    "wanwan",
+    "nya-",
+    "gaogao",
+    "Wow"
+]
+
+print(spells[3])
+
+print("-----辞書-----")
+
+quotes = {
+    "name": "wanwan",
+    "age" : 33
+}
+stooge = "name"
+print(quotes)
+print(quotes[stooge])
